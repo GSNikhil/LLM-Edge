36a2e3e6ef7b67da0fa5044f57f647dc080d3df6
Nikhil G S <gsnikhil333@gmail.com>
2025-03-09 18:31:36 -0700

    Update run.py

--- run.py
+++ run.py
@@ -1,8 +1,18 @@
 import evaluate_llm as eval
 from transformers import AutoModelForCausalLM, AutoTokenizer
+import time
+import torch
 
 model_name = "./Qwen2.5-0.5B"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-eval.measure_test_accuracy(model, tokenizer, "./gsm8k_saved", 'cuda', display=True)
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model.to(device)
+
+start_time = time.perf_counter()
+eval.measure_test_accuracy(model, tokenizer, "./gsm8k_saved", device, display=True)
+end_time = time.perf_counter()
+
+execution_time = end_time - start_time
+print(f"Execution Time: {execution_time:.6f} seconds")
